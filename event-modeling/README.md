4b81ffeb68d7987fedf6268c3be8dea6cf4663a8
Ladislav Gazo <ladislav.gazo@gmail.com>
2025-02-04 20:12:51 +0100

    feat: first version of the DSL without model entity cross-reference checking

---
+++ event-modeling/README.md
@@ -0,0 +1,16 @@
+# Development
+
+In one terminal watch for changes in the grammar file:
+
+```bash
+pnpm run langium:watch
+```
+
+In the second terminal watch for code changes reflected in Monaco web editor:
+
+```bash
+pnpm run dev
+```
+
+The editor is available at: http://192.168.100.12:5173/static/monacoExtended.html
+
---
+++ event-modeling/src/language/event-modeling-validator.ts
@@ -0,0 +1,51 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { EventModelingAstType, Frame } from './generated/ast.js';
+import type { EventModelingServices } from './event-modeling-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: EventModelingServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.EventModelingValidator;
+    const checks: ValidationChecks<EventModelingAstType> = {
+        // Person: validator.checkPersonStartsWithCapital
+        Frame: validator.checkFrameEntityExists
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class EventModelingValidator {
+
+    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    //     if (person.name) {
+    //         const firstChar = person.name.substring(0, 1);
+    //         if (firstChar.toUpperCase() !== firstChar) {
+    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //         }
+    //     }
+    // }
+
+    checkFrameEntityExists(frame: Frame, accept: ValidationAcceptor): void {
+        /**
+         * If we put cross-reference check there, then we need to implement the validator the following way. The question remains how to supress the reference check itself.
+         *
+Frame:
+    'tf' name=FI modelEntityType=ModelEntityType entityIdentifier=[ModelEntity:EID] ('>t' sourceFrame=[Frame:FI])? ('[[' dataReference=[DataEntity:EID] ']]')? DataBlock;
+        */
+
+        // console.log(`hello `,frame);
+        // if (frame.entityIdentifier && !this.getEntityByName(frame.entityIdentifier.$refText)) {
+        //     accept('info', `Entity '${frame.entityIdentifier.$refText}' does not exist.`, { node: frame, property: 'entityIdentifier' });
+        // }
+    }
+
+    getEntityByName(name: string | undefined) {
+        if (!name) return undefined;
+        // Add logic to search for the entity in the model
+        return undefined; // Return the entity if found, otherwise return undefined
+    }
+}
