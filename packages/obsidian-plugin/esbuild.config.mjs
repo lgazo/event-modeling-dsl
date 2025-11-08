f6dbd64d397ee89bc47fd4b6c53f907f03fefb81
Ladislav Gazo <ladislav.gazo@gmail.com>
2025-11-08 10:59:27 +0100

    chore(obsidian-plugin): aligned the structure and content with the sample plugin

--- packages/obsidian-plugin/esbuild.config.mjs
+++ packages/obsidian-plugin/esbuild.config.mjs
@@ -1,4 +1,6 @@
 import esbuild from 'esbuild';
+import process from "process";
+import builtins from "builtin-modules";
 
 const args = process.argv.slice(2);
 const isWatch = args.includes('--watch');
@@ -14,22 +16,37 @@ const options = {
   format: 'cjs',
   sourcemap: isProd ? false : 'inline',
   minify: isProd,
-  external: ['obsidian'],
   treeShaking: true,
   // loader: {
   //   ".node": "file",
   // },
   outdir: isProd ? "out" : "out/test-vault/.obsidian/plugins/event-modeling-obsidian-plugin/",  // Output to plugin dir for dev; use a build dir for prod
   // outdir: "out"
+  external: [
+    "obsidian",
+    "electron",
+    "@codemirror/autocomplete",
+    "@codemirror/collab",
+    "@codemirror/commands",
+    "@codemirror/language",
+    "@codemirror/lint",
+    "@codemirror/search",
+    "@codemirror/state",
+    "@codemirror/view",
+    "@lezer/common",
+    "@lezer/highlight",
+    "@lezer/lr",
+    ...builtins],
 };
 
 async function run() {
+  const ctx = await esbuild.context(options);
   if (isWatch) {
-    const ctx = await esbuild.context(options);
     await ctx.watch();
     console.log('watching for changes...');
   } else {
-    await esbuild.build(options);
+    await ctx.rebuild();
+    process.exit(0);
   }
 }
 
